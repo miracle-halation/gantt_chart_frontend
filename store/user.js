f4ddb9d0b6919fffcc067830586b3f7ed3b96770
miracle-halation <miraclehalation.9710@gmail.com>
2022-07-21 21:44:04 +0900

    cookieに必要な値を保存
    名前をわかりづらいものに変更

--- store/user.js
+++ store/user.js
@@ -1,18 +1,15 @@
+import Cookies from "universal-cookie"
+const cookies = new Cookies()
+
 export const state = () => ({
 	isLoggedIn: false,
 	user: null,
-	uid : null,
-	access_token: null,
-	client: null,
 	icon: null
 })
 
 export const getters = {
 	isLoggedIn: (state) => state.isLoggedIn,
 	user: (state) => state.user,
-	uid: (state) => state.uid,
-	access_token: (state) => state.access_token,
-	client: (state) => state.client,
 	icon: (state) => state.icon
 }
 
@@ -22,20 +19,10 @@ export const mutations = {
 		state.user = user
 		state.icon = icon
 	},
-	setToken(state, {uid, access_token, client}) {
-		state.uid = uid
-		state.access_token = access_token
-		state.client = client
-	},
 	LogoutUser (state) {
 		state.isLoggedIn = !state.isLoggedIn
 		state.user = null
 		state.icon = null
-	},
-	removeToken(state) {
-		state.uid = null
-		state.access_token = null
-		state.client = null
 	}
 }
 
@@ -46,7 +33,10 @@ export const actions = {
 			const user_data = response.data.data
 			const user_headers = response.headers
 			commit('setUser', {user: user_data, icon: null})
-			commit('setToken', {uid: user_data['uid'], access_token: user_headers['access-token'], client: user_headers['client']})
+			cookies.set('isLogin', true)
+			cookies.set('id-u', user_data['uid'])
+			cookies.set('tk-pass', user_headers['access-token'])
+			cookies.set('cli-us', user_headers['client'])
 			this.$router.push('/')
 		}).catch((err) => {
 			console.log(err)
@@ -54,16 +44,18 @@ export const actions = {
 	},
 	async logout({commit, getters}){
 		const headers = {
-			uid: getters.uid,
-			access_token: getters.access_token,
-			client: getters.client,
+			uid: cookies.get('id-u'),
+			access_token: cookies.get('tk-pass'),
+			client: cookies.get('cli-us')
 		}
 		await this.$axios.delete('/v1/auth/sign_out', {headers: headers})
 		.then((response) => {
 			commit('LogoutUser')
-			commit('removeToken')
+			cookies.remove('id-u')
+			cookies.remove('tk-pass')
+			cookies.remove('cli-us')
 		}).catch((err) => {
 			console.log(err)
 		})
-	}
+	},
 }
